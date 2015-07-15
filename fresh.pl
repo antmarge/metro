8a45189e9f49475ac6b4553222a1e948e1893bd1
Margaret Antonio <margaretantoniO@gmail.com>
2015-07-15 13:10:39 -0400

    Sorted files before comparing

--- fresh.pl
+++ fresh.pl
@@ -4,7 +4,9 @@
 
 #Script to create a .csv file of fold change comparisons that can be inputed into R for rank comparison statistical analysis
 
-#perl prep.pl <genelistFile.txt> <First.csv> <Second.csv>
+#perl prep.pl --list <genelistFile.txt> --L1 first,second --L2 first,second <tracking file>
+
+#perl fresh.pl --L1 21,33 --L2 25,33 -h --list ../../testGetMatrix/9_rankGenes/common_5322-5324.txt ../../testGetMatrix/genes.fpkm_tracking 
 
 #list of common genes should include only ones with
 #--output-file is the equivalent of -o
@@ -12,97 +14,96 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Tie::IxHash;
 
-our ($one, $list,$two);
+our ($one, $list,$two,$h);
 GetOptions(
 'L1:s' => \$one,
 'L2:s' => \$two,
 'list=s'=>\$list,
+'h'=>\$h,
 );
 
 
-my @L1=split(",",$one);
-my @L2=split(",",$two);
+sub printer{
+	print "--L1 \t enter entry number for first comparison sample1,sample2\n";
+	print "--L2 \t enter entry number for second comparison sample1,sample2\n";
+	print "--list \t name of file containing common genes between the two samples\n";
+}
+
+sub log2{
+    my $n = shift;
+    return log($n)/log(2);
+}
+#fix labels as arrays
+
+my @L1=split(",",$one);  #@L1=(21,33)
+my @L2=split(",",$two);  #@L2=(25,33)
 my $L1=\@L1;
 my $L2=\@L2;
-#my $list=$ARGV[0]; #input list of genes that two comparisons have in common (can use VennY with gene lists from crewcut.pl (gene_exp.diff)
-my $file1=$ARGV[0];
 
-print "These are the files: \n\tGene List File: $list\n\tCuffdiff file:$file1\n";
-print "\nFirst comparison labels: @L1\n Second comparison labels: @L2\n";
-
-open FH,'<',$list;
-
+#Read file with common genes into array
+open (FH,'<',$list) or print "Cannot open input tracking file" and die;
 my $string="";
-
 while (<FH>){
     $string.=$_;
 }
-
 close FH;
-#print $string, "\n"; GOOD
 my @common=split("\n",$string);
 my $common=\@common;
-#print @common;
 
-my @first;
-my @second;
+#Open tracking file to pull comparisons
+# [4]-GeneSymbol
+open (TR,'<', $ARGV[0]) or print "Cannot open input tracking file" and die;
+if ($h){
+	my $line=<TR>;
+	my @header=split("\t",$line);
+	print "Entry codes for tracking file:\n";
+	for (my $i=0;$i<scalar @header;$i++){
+		print $i,":\t",$header[$i],"\n";
+	}
+	printer();
+	die;
+}
+
+my $index=0;
+tie my %hash,"Tie::IxHash";
+
+my @whole; #have to read the whol gene.fpkm_tracking file into an array because need to sort it alphabetically
+
+open OUT,'>',"out.txt";
+
+while (my $line=<TR>){
+	#print "Got to while loop";
+	my @fields=split("\t",$line);
+	push (@whole,\@fields);
+	#print "Looking for: $common[$i]\n";
+}
+
+#Sort both lists
+my @swhole=sort { $a->[4] cmp $b->[4]} @whole;
+my @scommon=sort {$a cmp $b} @common;
 my $i=0;
-my %firsthash;
-my %sechash;
-
-#now have array of all common genes between two samples to be compared
-#GOAL: find these genes in the gene_exp.diff file for the epecified comparisons and make two columns with fold change of each gene (in same order as array)
-open LOG,'>', "run.log";
-
-open (FIRST,'<',$file1);
-$i=0;
-#my %hash;
-#keep index of array of common genes
-while (my $line = <FIRST>){
-    #print "Looking for: $common[$i]\n";
-    my @fields=split("\t",$line);
-    #my $fields=\@fields;
-    #print LOG "$fields[0]: $fields[4]----$L2[0] and $fields[5]-----$L2[1] and $fields[2] -----$common[$i]\n";
-    if (($fields[4] eq $L1[0]) and ($fields[5] eq $L1[1])){ #and ($fields[2] eq $common[$i])){
-        my @vals=split(",",$fields[2]);
-        my $vals=\@vals;
-        $firsthash{$vals[0]}=$fields[9];
-    }
+foreach my $t(@swhole){
+	my @t=@$t;
+	if ($t[4] eq "$common[$i]"){
+	#print "They're equal\n";
+	my $fc1=log2($t[$L1[0]]/$t[$L1[1]]);
+	my $fc2=log2($t[$L2[0]]/$t[$L2[1]]);
+	my @vals=($fc1,$fc2);
+	$hash{$t[4]}=@vals;
+	$i++;
+	#print "pushed";
 }
-close FIRST;
-
-
-
-#open file again to make second hash
-open (SEC,'<',$file1);
-$i=0;
-
-while (my $line=<SEC>){
-   my @fields=split ("\t",$line);
-    #print LOG "$fields[0]: $fields[4]----$L2[0] and $fields[5]-----$L2[1] and $fields[2] -----$common[$i]\n";
-    if (($fields[4] eq $L2[0]) and ($fields[5] eq $L2[1])){ #and ($fields[2] eq $common[$i])){
-        my @vals=split(",",$fields[2]);
-        my $vals=\@vals;
-        $sechash{$vals[0]}=$fields[9];
-   }
 }
-close SEC;
-
-open (CSV,'>',"combinedFC.csv");
-my @complete;
-for (my $x=0;$x<scalar @common;$x++){
-    my $val1=$firsthash{$common[$x]};
-    my $val2=$sechash{$common[$x]};
-    if (!$val1){$val1="ALERT";}
-    if (!$val2){$val2="ALERT2";}
-    
-    print CSV $common[$x],",",$val1,",",$val2,"\n";
+open CSV,'>',"out.csv";
+for (my($key,$value)=each %hash){
+	print CSV $key;
+	my @value=@$value;
+	foreach my $v(@value){
+		print CSV ",",$v;
+	}
+	print CSV "\n";
 }
+
 close CSV;
-#Check integrity of first hash
-#while (my ($key,$value)=each %sechash){
-#    print $key, "\t", $value,"\n";
-#}
-
-close LOG;
